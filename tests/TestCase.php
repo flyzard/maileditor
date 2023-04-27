0855d24d52324651bdf9eeeef6f8a043de0280ab
flyzard <flyzard@users.noreply.github.com>
2023-04-27 08:33:21 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,9 @@
 
 namespace Flyzard\Maileditor\Tests;
 
+use Flyzard\Maileditor\MaileditorServiceProvider;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
-use Flyzard\Maileditor\MaileditorServiceProvider;
 
 class TestCase extends Orchestra
 {
